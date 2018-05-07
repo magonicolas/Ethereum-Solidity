dbbe76836028fd37004e44df039d784c483cdbf1
Nicolas Palacios <magonicolas@gmail.com>
2018-05-07 15:19:28 -0300

    Adds a new Example.

---
+++ SavingsWithDate.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.19;
+
+contract Savings {
+    address owner;
+    uint256 deadline;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function deposit(uint256 amount) public payable {
+        require(msg.value == amount);
+    }
+
+    function Savings(uint256 numberOfDays) public payable {
+        owner = msg.sender;
+        deadline = now + (numberOfDays * 1 days);
+    }
+
+    function withdraw() public onlyOwner {
+        require(now >= deadline);
+
+        msg.sender.transfer(address(this).balance);
+    }
+}
